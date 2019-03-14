e8988bf5500661909c4f91e0bbb723a78f6e52f7
Ritayan Chakraborty <ritayanout@gmail.com>
2019-03-14 17:22:29 +0530

    Use java 1.8 features

---
+++ app/src/main/java/com/edge/starringharsh/EDGE/ui/ContactsView.java
@@ -0,0 +1,24 @@
+package com.edge.starringharsh.EDGE.ui;
+
+/*
+ * Copyright (C) 2019 Ritayan Chakraborty <ritayanout@gmail.com>
+ *
+ * This file is part of EDGE
+ *
+ * EDGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EDGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EDGE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+public class ContactsView {
+}
